23d2f38b63faa68f1690ab1e183563a6d84d1c09
Jef <89365867+JefAlmeida1@users.noreply.github.com>
2021-10-06 11:42:30 -0300

    passos javascript

--- JavaScript/aula1/man.js
+++ JavaScript/aula1/man.js
@@ -1,8 +1,8 @@
 var nome = "Aluno Js";
 var n1 = 5;
 var n2 = 3;
-var frase = "Japão é um pais"
+var frase = "Japão é um pais";
 //alert(nome + " tem " + idade + " anos");//
 console.log(nome);
 console.log(n1 + n2);
-console.log(frase.)
+console.log(frase.replace("Japão", "Brasil"));
