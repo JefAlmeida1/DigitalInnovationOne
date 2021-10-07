741580427a7b9ec57bc4f1b98ee53f6a52b6d878
Jef <89365867+JefAlmeida1@users.noreply.github.com>
2021-10-07 13:06:13 -0300

    aplicando funções em aula prática

--- JavaScript/aula3/funções.js
+++ JavaScript/aula3/funções.js
@@ -1,4 +1,20 @@
-function soma(n1, n2){
+/*function soma(n1, n2){
     return n1 + n2;
+}*/
+
+//alert(soma(5, 10));
+
+var validar;
+function validaIdade(idade){
+    validar;
+    if(idade >= 18){
+        validar = true
+    
+    }else{
+        validar= false
+    }
+    return validar;
 }
-alert(soma(5, 10));
+var idade = prompt("Qual sua idade?");
+validaIdade(idade)
+console.log(validar);
