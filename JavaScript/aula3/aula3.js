1abfde7bc2fc16d50180925ebe5dcab3ddae3a30
Jef <89365867+JefAlmeida1@users.noreply.github.com>
2021-10-07 14:20:03 -0300

    praticas 6

--- JavaScript/aula3/aula3.js
+++ JavaScript/aula3/aula3.js
@@ -1,3 +1,24 @@
 function clique(){
     document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar! </b>";
 }
+function redirecionar(){
+   // para abrir em outra aba:
+    window.open("https://www.google.com/");
+  // para abrir na mesma aba: window.location.href = "https://www.google.com/";
+}
+
+function trocar(elemento){
+    elemento.innerHTML = "Obrigado por passar o mouse!";
+    //alert("trocar texto");
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "passe o meuse aqui";
+}
+
+function load(){
+    alert("página carregada");
+}
+function funcaoChanger(elemento){
+    console.log(elemento.value)
+}
