b9a125983af87c45a4344248c3a5150098c6f224
Jef <89365867+JefAlmeida1@users.noreply.github.com>
2021-10-31 01:06:59 -0300

    o

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,1 +1,39 @@
-console.log('TypeScrips');
+ /*
+ interface IAnimal{
+     nome: string;
+     tipo: 'terrestre' | 'aquático';
+    executarRugido(alturaEmDecibies: number): void;
+ }
+
+
+
+ interface IFelino extends IAnimal{
+     visaoNoturna: boolean;
+ }
+ const IAnimal: IAnimal = {
+    noma: 'Elefante',
+    tipo: 'terrestre',
+    executarRugido: (alturaEmDecibies) => (`${alturaEmDecibies}dB`)
+ }
+
+ animal.executarRugido('s')
+
+ const felino: IFelino = {
+     nome: 'Leão',
+     tipo: 'terrestre',
+     visaoNorturna: true,
+ }
+
+ const input = document.getElementById('input') as HTMLInputElement;
+ input.addEventListener('input', (event) =>{
+     const i = event.currentTarget as HTMLInputElement;
+ });*/
+
+/*types genericos
+ function preencheLista<T>(array: any, valor: T){
+     return array.map(item => item + valor);
+ }
+ preencheLista(['A', 'B', 'C'], 'D');
+ */
+
+ 
