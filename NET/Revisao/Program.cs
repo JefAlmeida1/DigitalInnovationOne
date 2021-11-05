6fba4eb480e06215d4dd77114965a892493ad144
Jef <89365867+JefAlmeida1@users.noreply.github.com>
2021-11-05 09:33:00 -0300

    praticando durante a aula

---
+++ NET/Revisao/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1- Inserer novo aluno");
+            Console.WriteLine("2- Lista alunos");
+            Console.WriteLine("-3 Calcular média geral");
+            Console.WriteLine("X- Sarir");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            while(opcaoUsuario.ToUpper() != "X"){
+                switch(opcaoUsuario){
+                    case "1":
+                    //Adicionar alunos
+                    break;
+                    case "2":
+                    //Listar alunos
+                    break;
+                    case "3":
+                    //Calcular média
+                    break;
+                    default:
+                    throw new ArgumentOutOfRangeException();
+                }
+                 Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1- Inserer novo aluno");
+            Console.WriteLine("2- Lista alunos");
+            Console.WriteLine("-3 Calcular média geral");
+            Console.WriteLine("X- Sarir");
+            Console.WriteLine();
+
+            opcaoUsuario = Console.ReadLine();
+            }
+        }
+    }
+}
