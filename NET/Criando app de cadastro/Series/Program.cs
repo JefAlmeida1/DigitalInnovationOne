7257d44b0c4962c4908fc567b5c1af920c99e9fd
Jef <89365867+JefAlmeida1@users.noreply.github.com>
2021-11-06 22:25:32 -0300

    Inicicio de projeto prático

---
+++ NET/Criando app de cadastro/Series/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Series meuObjeto = new Series();
+            
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
