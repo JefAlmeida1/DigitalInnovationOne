d1c7155820ce7033a238f5bcee3fbc9bab9542be
Jef <89365867+JefAlmeida1@users.noreply.github.com>
2021-11-17 17:51:46 -0300

    react

---
+++ ReactJS/Componentes/react-aula/Components/Item/index.js
@@ -0,0 +1,9 @@
+const Item = () =>{
+    return(
+        <a href="#" class="list-group-item list-group-item-action list-group-item-dark">
+            {props.childrem}
+        </a>
+    )
+}
+
+export default Item;
---
+++ ReactJS/Componentes/react-aula/src/App.js
@@ -0,0 +1,17 @@
+import Item from '../Components/Item/index';
+
+const App = () =>{
+  return(
+    <div>
+      <h1>Olá mundo</h1>
+      <ul>
+        <Item>
+          Item 1
+        </Item>
+        
+      </ul>
+
+    </div>
+  )
+}
+export default App;
