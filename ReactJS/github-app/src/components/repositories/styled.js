bc994f04a7e183e5d655b66379a2c9e4b6320923
Jef <89365867+JefAlmeida1@users.noreply.github.com>
2021-11-21 18:42:19 -0300

    ..

--- ReactJS/github-app/src/components/repositories/styled.js
+++ ReactJS/github-app/src/components/repositories/styled.js
@@ -2,53 +2,53 @@ import styled from "styled-components";
 import { Tabs, TabList, Tab, TabPanel } from "react-tabs";
 
 export const WrapperTabs = styled(Tabs)`
-    font-size: 16px;
-    width: 50%;
+  font-size: 16px;
+  width: 100%;
+  margin-top: 16px;
 `;
 
 export const WrapperTabList = styled(TabList)`
-    list-style-type: nome;
-    padding: 4px;
-    display: flex;
-    matgin: 0px;
+  list-style-type: none;
+  padding: 4px;
+  display: flex;
+  margin: 0;
 `;
 WrapperTabList.tabsRole = "TabList";
 
 export const WrapperTab = styled(Tab)`
-    borde-radius: 16px;
-    border: 1px solid #ccc;
-    padding: 8px;
-    user-select: none;
-    cursor: pointer;
-    z-index: 99999;
-    background-color: "#fff";
-    margin: 8px;
-  
-    &:focus {
-      outline: none;
-    }
-  
-    &.is-selected {
-      box-shadow: 3px 2px 10px rgba(0, 0, 0, 0.2);
-    }
-`;
-WrapperTabList.tabsRole = "Tab";
+  border-radius: 16px;
+  border: 1px solid #ccc;
+  padding: 16px;
+  user-select: none;
+  cursor: pointer;
+  z-index: 99999;
+  background-color: "#fff";
+  margin: 8px;
 
-export const WrapperabPanel = styled(TabPanel)`
-    padding: 16px;
-    border: 1px solid "#ccc";
-    display: none;
-    margin-top: -5px;
+  &:focus {
+    outline: none;
+  }
 
-    &.is-selected {
-        display: block;
-    }
+  &.is-selected {
+    box-shadow: 3px 2px 10px rgba(0, 0, 0, 0.2);
+  }
 `;
-WrapperTabList.tabsRole = "TabPanel";
+WrapperTab.tabsRole = "Tab";
+
+export const WrapperTabPanel = styled(TabPanel)`
+  padding: 16px;
+  border: 1px solid "#ccc";
+  display: none;
+  margin-top: -5px;
+
+  &.is-selected {
+    display: block;
+  }
+`;
+WrapperTabPanel.tabsRole = "TabPanel";
 
 export const WrapperList = styled.div`
-    display: flex;
-    justify-content: flex-start;
-    flex-wrap: wrap;
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
 `;
-
