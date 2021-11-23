8bfc697c4adc76e9cdc11035b21f8750e0c97460
Jef <89365867+JefAlmeida1@users.noreply.github.com>
2021-11-23 15:09:58 -0300

    alteração de cor

--- ReactJS/github-app/src/components/header/styled.js
+++ ReactJS/github-app/src/components/header/styled.js
@@ -16,13 +16,13 @@ input {
 }
 
 button {
-  background-color: #225ed8;
-  padding: 8px 16px;
+  background-color: red;
+  padding: 10px 16px;
   margin: 0 16px;
   border-radius: 8px;
 
   &:hover {
-    background-color: #2c5282;
+    background-color: black;
     box-shadow: 3px 2px 10px rgba(0, 0, 0, 0.2);
   }
 
