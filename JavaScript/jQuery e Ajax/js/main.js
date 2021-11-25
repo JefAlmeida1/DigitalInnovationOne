f58ee1d5a5985ec29a0a79893b5de6ae0dddae38
Jef <89365867+JefAlmeida1@users.noreply.github.com>
2021-11-25 13:46:26 -0300

    praticando jquery

---
+++ JavaScript/jQuery e Ajax/js/main.js
@@ -0,0 +1,9 @@
+function consultacep(){
+    $.ajax({
+        url: "https://viacep.com.br/ws/14800390/json/",
+        type: "GET",
+        sucess: function(response){
+            console.log(response);
+        }
+    })
+}
