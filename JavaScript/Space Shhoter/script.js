13ca3620c6106293b469ab384a5e73ae5931a646
Jef <89365867+JefAlmeida1@users.noreply.github.com>
2021-11-30 13:24:14 -0300

    a

--- JavaScript/Space Shhoter/script.js
+++ JavaScript/Space Shhoter/script.js
@@ -26,3 +26,19 @@ function moveUp(){
         yourShip.style.top = `${position}px`
     }
 }
+
+//down
+
+function moveDown(){
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top')
+    if(topPosition === "540px"){
+        return
+    }else{
+        let position = parseInt(topPosition)
+        position += 50
+        yourShip.style.top = `${position}px`
+    }
+    
+}
+
+window.addEventListener('keydown', flyShip)
