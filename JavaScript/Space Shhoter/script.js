094bacdb783424945ca5c684a9bbb5d292c187a9
Jef <89365867+JefAlmeida1@users.noreply.github.com>
2021-11-30 16:17:39 -0300

    f

--- JavaScript/Space Shhoter/script.js
+++ JavaScript/Space Shhoter/script.js
@@ -1,5 +1,9 @@
-constyourShip = document.querySelector('.player-shhoter')
+const yourShip = document.querySelector('.player-shhoter')
 const playArea = document.querySelector('#main-play-area')
+const aliensImg = ['img/monster-1.png', 'img/monster-2.png', 'img/monster-3.png']
+const instructionsText = document.querySelector('.game-instructions')
+const startButton = document.querySelector('.start-button')
+let alienInterval
 
 function flyShip(event){
     if(event.key === 'ArrowUp'){
@@ -40,5 +44,124 @@ function moveDown(){
     }
     
 }
+//tiros
+function fireLaser(){
+    let laser = createLaserElement()
+    playArea.appendChild(laser)
+    moveLaser(laser)
+}
 
-window.addEventListener('keydown', flyShip)
+function createLaserElement(){
+    let xPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('left'))
+    let yPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('top'))
+    let newLaser = document.createElement('img')
+    newLaser.src = 'img/shoot.png'
+    newLaser.classList.add('laser')
+    newLaser.style.left = `${xPosition}px`
+    newLaser.style.top = `${yPosition - 10}px`
+    return newLaser;
+}
+
+function moveLaser(laser){
+    let laserInterval = setInterval(()=>{
+        let xPosition = parseInt(laser.style.left)
+        let aliens = document.querySelectorAll('.alien')
+
+        aliens.forEach((alien) =>{
+            if(checkLaserCollision(laser, alien)){
+                alien.src = 'img/explosion.png'
+                alien.classList.remove('alien')
+                alien.classList.add('dead-alien')
+            }
+        })
+
+        if(xPosition === 340){
+            laser.remover()
+        }else{
+            laser.style.left = `{${xPosition + 8}px}`
+        }
+    }, 10)
+}
+
+//criar inimigos
+function createAliens(){
+    let newAlien = document.createElement('img')
+    let aliensSprite = aliensImg[Math.floor(Math.random() * aliensImg.length)]
+    newAlien.src = aliensSprite
+    newAlien.classList.add('alien')
+    newAlien.classList.add('alien-transition')
+    newAlien.style.left = '370px'
+    newAlien.style.top = `${Math.floor(Math.random()* 330) + 30}px`
+    playArea.appendChild(newAlien)
+    moveAlien(newAlien)
+}
+
+//move inimigos
+function moveAlien(alien){
+    let moveAlienInterval = setInterval(() =>{
+        let xPosition = parseInt(window.getComputedStyle(alien).getPropertyValue('left'))
+        if(xPosition <= 50){
+            if(Array.from(alien.classList).includes('dead-alien')){
+                alien.remove()
+            }else {
+                gameOver()
+            }
+            }else{
+                alien.style.left = `{xPosition - 4}px`
+        }
+    }, 30)
+}
+
+//colisão
+
+function checkLaserCollision(laser, alien){
+    let laserTop = parseInt(laser.style.top)
+    let laserLeft = parseInt(laser.style.left)
+    let laserBottom = laserTop - 20
+    let alienTop = parseInt(alien.style.top)
+    let alienLeft = parseInt(alien.style.left)
+    let alienBottom = alienTop - 30
+    if(laserLeft !=304 && laserLeft + 40 >= alienLeft){
+        if(laserTop <= alienTop && laserTop >= alienBottom){
+            return true
+        }else{
+            return false
+        }
+    }else{
+        return false
+    }
+}
+
+//inicio
+
+startButton.addEventListener('click', (event) =>{
+    playGame()
+})
+
+function playGame(){
+    startButton.style.display = 'none'
+    instructionsText.style.display = 'none'
+    window.addEventListener('keydown', flyShip)
+    alienInterval = setInterval(() =>{
+        createAliens()
+    }, 2000)
+
+}
+
+//game over
+
+function gameOver(){
+    window.removeEventListener('keydown', flyShip)
+    clearInterval(alienInterval)
+    let aliens = document.querySelectorAll('.alien')
+    aliens.forEach((alien) => alien.remove())
+    let lasers = document.querySelectorAll('.laser')
+    lasers.forEach((laser) => laser.remove())
+    serTimeout(() =>{
+        alert('game over!')
+        yourShip.style.top = "250px"
+        startButton.style.display = "block"
+        instructionsText.style.display = "block"
+    })
+
+}
