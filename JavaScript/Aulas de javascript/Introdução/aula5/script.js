4dec1ea96d04a3abc25103aa221f2c031d0a6031
Jef <89365867+JefAlmeida1@users.noreply.github.com>
2021-12-03 16:05:06 -0300

    aula 5

---
+++ JavaScript/Aulas de javascript/Introdução/aula5/script.js
@@ -0,0 +1,12 @@
+var numero = document.getElementById('numero')
+var numero = 0
+
+function increment(){
+    numero = numero + 1
+    numeroWrapper.innerHTML  = numero
+}
+
+function sub(){
+    numero = numero - 1
+    numeroWrapper.innerHTML  = numero
+}
