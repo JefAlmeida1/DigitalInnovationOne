513ae552bc2471a6b9eb2bb70c61aa358b25e341
Jef <89365867+JefAlmeida1@users.noreply.github.com>
2022-03-23 18:09:25 -0300

    00

--- ReactJS/Mentoria 04 Projeto Eduzz Componentizando a aplicação/api/src/controllers.ts
+++ ReactJS/Mentoria 04 Projeto Eduzz Componentizando a aplicação/api/src/controllers.ts
@@ -8,3 +8,11 @@ export const pingResult = (req: Request, res: Response) =>  {
 
     })
 };
+
+export const pingResultv2 = (req: Request, res: Response) =>  {
+    res.status(201).json({
+        message:"Pong",
+        body: {...req.body}
+
+    })
+};
--- ReactJS/Mentoria 04 Projeto Eduzz Componentizando a aplicação/api/src/index.ts
+++ ReactJS/Mentoria 04 Projeto Eduzz Componentizando a aplicação/api/src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import { pingResult } from './controllers';
+import { pingResult, pingResultv2 } from './controllers';
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/ping', pingResult);
+app.post('/v1/ping', pingResult);
+app.post('/v2/ping', pingResultv2);
 
 app.listen(8000, () => {
     console.log("Servidor na porta 8000...");
