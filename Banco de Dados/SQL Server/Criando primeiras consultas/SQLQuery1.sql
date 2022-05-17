17cf6d6f931d87eb57846b06db2dbc3724f8fea9
Jef <89365867+JefAlmeida1@users.noreply.github.com>
2022-05-17 19:16:00 -0300

    mod

--- Banco de Dados/SQL Server/Criando primeiras consultas/SQLQuery1.sql
+++ Banco de Dados/SQL Server/Criando primeiras consultas/SQLQuery1.sql
@@ -1,7 +1,16 @@
-create table Produtos
+create table Pedido
 (
-Codigo int,
-Nome varchar(100),
-Descricao varchar(200),
-Preco float
+Codigo int not null,
+DataSolicitacao datetime not null,
+FlagPago bit not null,
+TotalPedido float not null,
+CodigoCliente int not null
+)
+
+create table PedidoItem
+(
+CodigoPedido int not null,
+CodigoProduto int not null,
+Preco float not null,
+Quantidade int not null
 )
